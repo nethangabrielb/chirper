8d9eb54c43f8afaa0acf1fa411f800e3b1c8f70d
nethangabrielb <bagasbas.nethangabriel@gmail.com>
2026-02-25 16:05:11 +0800

    chore(onboarding): simplify user fetching logic and remove unnecessary state updates

--- apps/frontend/src/app/onboarding/page.tsx
+++ apps/frontend/src/app/onboarding/page.tsx
@@ -2,39 +2,20 @@
 
 import ConfirmForm from "@/app/onboarding/components/form";
 import useUser from "@/stores/user.store";
-import { useQuery } from "@tanstack/react-query";
 
 import { redirect } from "next/navigation";
 
-const Onboarding = () => {
-  const setUser = useUser((state) => state.setUser);
-  const { data: user } = useQuery({
-    queryKey: ["currentUser"],
-    queryFn: async () => {
-      const res = await fetch(
-        `${process.env.NEXT_PUBLIC_API}/api/users?current=true`,
-        {
-          credentials: "include",
-        },
-      );
+import { User } from "@/types/user";
 
-      if (!res.ok) {
-        throw new Error("Error fetching from the server.");
-      }
-      const data = await res.json();
-      const user = data.data;
-      return user;
-    },
-    refetchOnWindowFocus: false,
-  });
+const Onboarding = () => {
+  const user = useUser((state) => state.user) as User;
 
   if (user) {
     if (user?.onboarded) {
-      setUser(user);
       redirect(`/home`);
     } else {
       // onboarding page should fetch user info using token
-      return <ConfirmForm user={user} setUser={setUser}></ConfirmForm>;
+      return <ConfirmForm user={user}></ConfirmForm>;
     }
   }
 };
